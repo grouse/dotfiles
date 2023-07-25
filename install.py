a8aed67afb69000ceb44e6ba7533a6a2d4323359
Jesper Stefansson <jesper.stefansson@gmail.com>
2023-07-25 15:40:48 +0200

    add linux side of install.py

--- install.py
+++ install.py
@@ -8,14 +8,25 @@ parser.add_argument("-c", "--configure", choices=["all", "neovim"], help="instal
 args = parser.parse_args();
 
 root = os.path.dirname(os.path.abspath(__file__))
-local_config = os.path.realpath(os.path.expandvars("%appdata%/../local"));
+
+
+if platform == "win32":
+    local_config = os.path.realpath(os.path.expandvars("%appdata%/../local"))
+elif platform == "linux":
+    local_config = os.path.realpath(os.path.expandvars("$HOME/.config/"))
+    if not os.path.exists(local_config):
+        mkdir(local_config)
+else:
+    print("unknown platform: {}".format(platform))
 
 def symlink(path, target):
-    if platform == "win32":
-        path = os.path.abspath(path)
-        target = os.path.abspath(target)
+    path = os.path.abspath(path)
+    target = os.path.abspath(target)
 
+    if platform == "win32":
         os.system("mklink /J {} {}".format(path, target))
+    elif platform == "linux":
+        os.system("ln -s {} {}".format(target, path))
 
 if args.configure == "all" or args.configure == "neovim":
     symlink("{}/nvim".format(local_config), "{}/neovim".format(root));
