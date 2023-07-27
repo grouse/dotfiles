67235c5b850f74d720ed5e63f4467bc61665d528
Jesper Stefansson <jesper.stefansson@gmail.com>
2023-07-27 13:17:00 +0200

    aaaall of the things. being a good boy about using plugin manager for all the license requirements and shit

--- neovim/init.lua
+++ neovim/init.lua
@@ -1,6 +1,40 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
 if vim.loop.os_uname().sysname == "Windows_NT" then
     vim.g.win32 = true
 end
+
+require("lazy").setup({
+    { "echasnovski/mini.align",      version = "*" },
+    { "echasnovski/mini.comment",    version = "*" },
+    { "echasnovski/mini.jump",       version = "*" },
+    { "echasnovski/mini.move",       version = "*" },
+    { "echasnovski/mini.statusline", version = "*", enabled = not vim.g.vsode },
+    { "echasnovski/mini.tabline",    version = "*", enabled = not vim.g.vsode },
+    { 'echasnovski/mini.sessions',   version = '*', enabled = not vim.g.vscode },
+ 	{ 'echasnovski/mini.starter',    version = '*', enabled = not vim.g.vscode },
+
+    { "neovim/nvim-lspconfig",                    enabled = not vim.g.vsode },
+    { "williamboman/mason.nvim",                  enabled = not vim.g.vsode },
+    { "williamboman/mason-lspconfig.nvim",        enabled = not vim.g.vsode },
+    { "akinsho/nvim-toggleterm.lua",              enabled = not vim.g.vsode },
+    { "rebelot/kanagawa.nvim",                    enabled = not vim.g.vsode },
+    { "nvim-lualine/lualine.nvim",                enabled = not vim.g.vsode },
+    { "nvim-treesitter/nvim-treesitter",          enabled = not vim.g.vsode,  build = ":TSUpdate" },
+    { "nvim-telescope/telescope.nvim",            enabled = not vim.g.vscode, tag = "0.1.2", dependencies = { "nvim-lua/plenary.nvim" } },
+    { "nvim-telescope/telescope-fzy-native.nvim", enabled = not vim.g.vsode },
+})
 
 require("mini.align").setup()
 require("mini.comment").setup()
@@ -8,19 +42,27 @@ require("mini.jump").setup()
 require("mini.move").setup()
 
 if not vim.g.vscode then
+    require("mason").setup()
+    require("mason-lspconfig").setup({
+        ensure_installed = { "clangd" }
+    })
+
+    local lsp = require("lspconfig")
+    lsp.clangd.setup({})
+
     require("toggleterm").setup({
         start_in_insert = false,
     })
 
-    require("vstask").setup({
-        terminal = "toggleterm",
-        term_opts = {
-            current = {
-                direction = "horizontal",
-                size = "15"
-            }
-        }
-    })
+    -- require("vstask").setup({
+    --     terminal = "toggleterm",
+    --     term_opts = {
+    --         current = {
+    --             direction = "horizontal",
+    --             size = "15"
+    --         }
+    --     }
+    -- })
 
     require("kanagawa").setup()
     vim.cmd.colorscheme("kanagawa-wave")
@@ -29,18 +71,23 @@ if not vim.g.vscode then
 
     require("telescope").setup()
     require("telescope").load_extension("fzy_native")
-    require("telescope").load_extension("vstask")
+    -- require("telescope").load_extension("vstask")
 
     if not vim.g.win32 then
-        require("mini.animate").setup()
+        --require("mini.animate").setup()
     end
 
     require("mini.statusline").setup()
     require("mini.tabline").setup()
+    require("mini.sessions").setup({
+        directory = vim.fn.stdpath("data") .. "/session",
+        file = "",
+    })
+    require("mini.starter").setup()
 
     require("nvim-treesitter.install").prefer_git = false
     require("nvim-treesitter.configs").setup {
-        ensure_installed = { "comment", "c", "cpp", "bash", "vim" },
+        ensure_installed = { "comment", "c", "cpp", "bash", "vim", "lua" },
         auto_install = false,
         highlight = {
             enable = true,
@@ -50,13 +97,15 @@ if not vim.g.vscode then
     require("nvim-treesitter").setup()
 end
 
-vim.opt.clipboard:append{ 'unnamedplus' }
+vim.lsp.handlers["textDocument/publishDiagnostics"] = function() end
 
+vim.opt.clipboard:append{ 'unnamedplus' }
 vim.opt.ignorecase=true
 vim.opt.smartcase=true
 vim.opt.cursorline=true
 vim.opt.inccommand="split"
-
+vim.opt.scrolloff=5
+vim.opt.sidescrolloff=5
 vim.opt.expandtab=true
 vim.opt.shiftwidth=4
 vim.opt.softtabstop=0
@@ -64,6 +113,17 @@ vim.opt.tabstop=4
 
 if vim.g.vscode then
 	vim.opt.inccommand="nosplit"
+end
+
+if vim.g.neovide then
+    vim.o.guifont = "UbuntuMono Nerd Font:h14"
+
+    vim.g.neovide_cursor_animation_length = 0
+    vim.g.neovide_cursor_animate_in_insert_mode = false
+    vim.g.neovide_cursor_animate_command_line = false
+    vim.g.neovide_scroll_animation_length = 0
+
+    vim.keymap.set({"n", "i"}, "<C-s>", ":w<CR>")
 end
 
 vim.keymap.set('v', '<', '<gv')
@@ -80,12 +140,36 @@ if not vim.g.vscode then
 end
 
 vim.cmd.highlight({"HighlightYank", "guifg=#5fb3b3"})
-
 vim.api.nvim_create_autocmd('TextYankPost', {
   group = vim.api.nvim_create_augroup('highlight_yank', {}),
   desc = 'Hightlight selection on yank',
   pattern = '*',
   callback = function()
     vim.highlight.on_yank { higroup = 'HighlightYank', timeout = 500 }
+  end,
+})
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    -- Enable completion triggered by <c-x><c-o>
+    --vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+    -- Buffer local mappings.
+    local opts = { buffer = ev.buf }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+
+    --vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+    --vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+    --vim.keymap.set('n', '<space>wl', function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, opts)
+    --vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+    --vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, opts)
   end,
 })
